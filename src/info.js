f9d9c6c52ba547be3f1ef3c8e60e5acb03c54719
Lucas Fernando Amorim <lf.amorim@yahoo.com.br>
2019-05-06 16:37:52 -0300

    feature: tribunais compativeis

--- src/info.js
+++ src/info.js
@@ -1,6 +1,17 @@
 import Parser from './parser';
 
 export default class Info extends Parser {
+  static compatible(info, procNumber, fieldName = 'numero_processo') {
+    return info.filter(({ fields }) => fields
+      .filter(({ name }) => (name === fieldName)).filter(({ otherMasks, mask }) => {
+        const masks = [mask].concat(otherMasks);
+        return masks.map(x => new RegExp(`^${x
+          .replace(/(\.)/g, '\\$1')
+          .replace(/N+/g, '\\d+')
+          .replace(/[A-Z]/g, '\\d')}$`).test(procNumber)).find(x => (x === true));
+      }).length);
+  }
+
   dump() {
     const { $ } = this;
     return $('database').map((i, databaseNode) => $('table', databaseNode).map((it, tableNode) => ({
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -5,7 +5,7 @@ const JuristekParser = require('../lib/index.js');
 describe('read processos', () => {
   it('parse processo.xml', () => {
     const processoStr = fs.readFileSync(path.join(__dirname, './info.xml'));
-    const processos = JuristekParser.Parser.openString(processoStr, JuristekParser.Info);
-    console.log(JSON.stringify(processos.dump(), null, 4));
+    const info = JuristekParser.Parser.openString(processoStr, JuristekParser.Info).dump();
+    console.log(JuristekParser.Info.compatible(info, '0001318-78.2015.5.02.0012'));
   });
 });
